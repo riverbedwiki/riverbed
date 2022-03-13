fc7a11c591f073a5d10c1545cfb74353b89ea4a1
cichol <cichol@live.cn>
2022-03-13 01:20:27 -0400

    add require admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_13_035403) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_13_051513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_13_035403) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["username"], name: "index_members_on_username", unique: true
   end
