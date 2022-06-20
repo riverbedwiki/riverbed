66074f2233efa917edee94a497908cc0c5aa53ee
cichol <cichol@live.cn>
2022-06-20 14:02:56 -0300

    add question model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_18_215430) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_20_165525) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_18_215430) do
   end
 
   create_table "propositions", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
     t.string "title", null: false
     t.text "body"
     t.datetime "created_at", null: false
